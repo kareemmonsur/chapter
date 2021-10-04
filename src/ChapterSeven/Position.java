4789307da9017ad54b928de4cb3e3b3bb53801e1
Monsur Kareem <monsurkareem@gmail.com>
2021-10-04 23:28:13 +0100

    newly done

---
+++ src/ChapterSeven/Position.java
@@ -0,0 +1,31 @@
+package ChapterSeven;
+
+public class Position {
+    private int rowPosition;
+    private int columnPosition;
+
+    public Position(int rowPosition, int columnPosition) {
+        this.rowPosition = rowPosition;
+        this.columnPosition = columnPosition;
+    }
+
+    public int getRowPosition() {
+        return rowPosition;
+    }
+
+    public void setRowPosition(int rowPosition) {
+        this.rowPosition = rowPosition;
+    }
+
+    public int getColumnPosition() {
+        return columnPosition;
+    }
+
+    public void setColumnPosition(int columnPosition) {
+        this.columnPosition = columnPosition;
+    }
+
+
+
+
+}
