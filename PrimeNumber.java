b8c6dd5c42bd1dd0ab6020dbaa5e46715bb8f9be
Monsur Kareem <monsurkareem@gmail.com>
2021-10-04 23:10:42 +0100

    newly done

---
+++ PrimeNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class PrimeNumber{
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int number, count, primeNumber;
+
+        number = input.nextInt();
+
+        if (count=1; count < number; count++){
+            System.out.println("Enter a number: ");
+
+            if (number % count == 0)
+                System.out.println("prime number");
+        }
+
+
+        }
+    }
+
+
+
+
+
+
