f2557fe2629a9c1b5a71392e42b6fc4dbe8c4986
Monsur Kareem <monsurkareem@gmail.com>
2021-10-05 11:53:51 +0100

    newly done

---
+++ src/AnArrayListTest.java
@@ -0,0 +1,12 @@
+public class AnArrayListTest implements List {
+    private boolean isEmpty = true;
+    @Override
+    public boolean isEmpty() {
+        return isEmpty;
+    }
+
+    @Override
+    public void add(int element) {
+
+    }
+}
