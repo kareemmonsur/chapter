be697e40387fb8ba52472c412612a4637fdfab81
Monsur Kareem <monsurkareem@gmail.com>
2021-10-05 19:43:28 +0100

    newly done

--- src/chapterThree/DaysOfTheWeek.java
+++ src/chapterThree/DaysOfTheWeek.java
@@ -10,26 +10,28 @@ public class DaysOfTheWeek {
 
         switch (weekNumber % 7) {
             case 0:
-                System.out.println("Sunday");
+                System.out.println("Day is Sunday");
                 break;
             case 1:
-                System.out.println("Monday");
+                System.out.println("Day is Monday");
                 break;
             case 2:
-                System.out.println("Tuesday");
+                System.out.println("Day is Tuesday");
                 break;
             case 3:
-                System.out.println("Wednesday");
+                System.out.println("Day is Wednesday");
                 break;
             case 4:
-                System.out.println("Thursday");
+                System.out.println("Day is Thursday");
                 break;
             case 5:
-                System.out.println("Friday");
+                System.out.println("Day is Friday");
                 break;
             case 6:
-                System.out.println("Saturday");
+                System.out.println("Day is Saturday");
                 break;
+
+
         }
 
     }
