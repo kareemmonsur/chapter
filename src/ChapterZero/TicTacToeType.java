5e3c1dc2f4771ff75280435048b873cc100b435c
Monsur Kareem <monsurkareem@gmail.com>
2021-10-07 06:45:03 +0100

    newly done

---
+++ src/ChapterZero/TicTacToeType.java
@@ -0,0 +1,22 @@
+package ChapterZero;
+
+public enum TicTacToeType {
+    EMPTY, X, O;
+
+    private static int value;
+    TicTacToeType() {
+
+    }
+
+    /*
+    create a class TicTacToe
+    TicTacToe has a board
+    enum X,O,EMPTY
+    constructor to initialize the board element as EMPTY
+    enum class two players
+    player1 move is == X
+    player@ move is == O
+
+
+     */
+}
