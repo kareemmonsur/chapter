c4b9bd4c99431440d6f5b279d912a0a8de20867c
Monsur Kareem <monsurkareem@gmail.com>
2021-10-14 22:35:44 +0100

    newly done

---
+++ src/ChapterSix/SumOfIntegerCalculatorTest.java
@@ -0,0 +1,8 @@
+package ChapterSix;
+
+public class SumOfIntegerCalculatorTest {
+    public static void main(String[] args) {
+        SumOfIntegerCalculator sum = new SumOfIntegerCalculator(4,2);
+        System.out.println(sum.getFirstNumber() + sum.getSecondNumber());
+    }
+}
