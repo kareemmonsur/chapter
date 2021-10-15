6a31df4dad05664c23fa6fa26b930550b982355a
Monsur Kareem <monsurkareem@gmail.com>
2021-10-15 06:14:17 +0100

    newly done

--- src/ChapterSix/SumOfIntegerCalculatorTest.java
+++ src/ChapterSix/SumOfIntegerCalculatorTest.java
@@ -2,8 +2,9 @@ package ChapterSix;
 
 public class SumOfIntegerCalculatorTest {
     public static void main(String[] args) {
-       SumOfIntegerCalculator sum = new SumOfIntegerCalculator(4,2);
+       SumOfIntegerCalculator sum = new SumOfIntegerCalculator();
        // System.out.println(sum.getFirstNumber() + sum.getSecondNumber());
+        sum.SumOfInteger(4,2);
         System.out.println(sum.getSum());
     }
 }
