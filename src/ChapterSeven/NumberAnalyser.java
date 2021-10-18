3fff641954797a5173ec5729b7d57597e8cbf390
Monsur Kareem <monsurkareem@gmail.com>
2021-10-18 13:38:40 +0100

    newly done

--- src/ChapterSeven/NumberAnalyser.java
+++ src/ChapterSeven/NumberAnalyser.java
@@ -7,11 +7,15 @@ public class NumberAnalyser {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter number of items you want to input: ");
         int itemNumber = input.nextInt();
+
         double [] items = new double[itemNumber];
 
         System.out.println("Enter numbers: ");
         for(int i= 0; i<= items.length; i++){
             items[i] = input.nextDouble();
+           // System.out.println(Arrays.toString(items));
         }
+
     }
+
 }
