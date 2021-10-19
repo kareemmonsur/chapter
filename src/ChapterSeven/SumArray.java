58c03cd1b57a5232fb85aaf689eb35c5ce951785
Monsur Kareem <monsurkareem@gmail.com>
2021-10-19 07:04:19 +0100

    newly done

--- src/ChapterSeven/SumArray.java
+++ src/ChapterSeven/SumArray.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class SumArray {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter total number of shoes: ");
+        System.out.print("Enter total number of shoes: ");
         int shoeNumber = input.nextInt();
 
         int [] shoeSizes = new int[shoeNumber];
@@ -14,8 +14,8 @@ public class SumArray {
         int i;
         int total = 0;
         for(i = 0; i < shoeSizes.length; i++){
-            total=+ shoeSizes[i];
-
+           shoeSizes[i] = input.nextInt();
+            total+= shoeSizes[i];
         }
         System.out.println(total);
         System.out.println(total/shoeNumber);
