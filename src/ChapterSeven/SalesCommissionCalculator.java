a8f2c3f5370bcc00d34d9dddf5e34a8e183dfee7
Monsur Kareem <monsurkareem@gmail.com>
2021-10-19 09:57:43 +0100

    newly done

--- src/ChapterSeven/SalesCommissionCalculator.java
+++ src/ChapterSeven/SalesCommissionCalculator.java
@@ -10,8 +10,10 @@ public class SalesCommissionCalculator {
 
         int [] grossPay = new int[numberOfWorkers];
 
+
         System.out.println("Enter gross pay of workers :");
-        for (int i = 0; i< grossPay.length; i++){
+        int i = 0;
+        for (i = 0; i< grossPay.length; i++){
             grossPay[i] = input.nextInt();
         }
 
