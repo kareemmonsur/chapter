6a8911599b45be45ee22ceb03f9c07bb166e3299
Monsur Kareem <monsurkareem@gmail.com>
2021-10-19 06:41:28 +0100

    newly done

---
+++ src/ChapterSeven/SumArray.java
@@ -0,0 +1,21 @@
+package ChapterSeven;
+
+import java.util.Scanner;
+
+public class SumArray {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter total number of shoes: ");
+        int shoeNumber = input.nextInt();
+
+        int [] shoeSizes = new int[shoeNumber];
+
+        System.out.println("Enter shoe sizes: ");
+        int i;
+        int total = 0;
+        for(i =0; i < shoeSizes.length; i++){
+
+
+        }
+    }
+}
