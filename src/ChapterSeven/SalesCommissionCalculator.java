c37790d36bb7686937a1f9a21f8b5597497c59dd
Monsur Kareem <monsurkareem@gmail.com>
2021-10-20 16:47:56 +0100

    newly done

--- src/ChapterSeven/SalesCommissionCalculator.java
+++ src/ChapterSeven/SalesCommissionCalculator.java
@@ -23,12 +23,26 @@ public class SalesCommissionCalculator {
         else if(salesCommission >= 300 || salesCommission < 400){
             counterB = counterB + 1;
         }
-        if(salesCommission >= 400 || salesCommission < 500){
+        else if(salesCommission >= 400 || salesCommission < 500){
             counterC = counterC + 1;
         }
-        if(salesCommission >= 500 || salesCommission < 600){
+        else if(salesCommission >= 500 || salesCommission < 600){
             counterD = counterD + 1;
         }
+        else if(salesCommission >= 600 || salesCommission < 700){
+            counterE = counterE + 1;
+        }
+        else if(salesCommission >= 700 || salesCommission < 800){
+            counterF = counterF + 1;
+        }
+        else if(salesCommission >= 800 || salesCommission < 900){
+            counterF = counterF + 1;
+        }
+        else(salesCommision >900)
+            counterG = counterG + 1;
+
+
+
         int counter = 1;
         for(Double pay:grossPay){
 
