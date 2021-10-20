9a7e42aabb45c23816cfd0e5132665e6e7a09926
Monsur Kareem <monsurkareem@gmail.com>
2021-10-20 22:37:54 +0100

    newly done

---
+++ src/ChapterSeven/PracticeArray.java
@@ -0,0 +1,13 @@
+package ChapterSeven;
+
+public class PracticeArray {
+    public static void main(String[] args) {
+        int [] items = {1,2,3,4,5};
+        int total= 0;
+        for(int i =0; i < items.length; i++){
+            System.out.println( items[i] + 2);
+        }
+
+    }
+
+}
