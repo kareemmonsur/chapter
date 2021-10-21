0d02d23271b43dcba9fb099877e0f0cf014c2182
Monsur Kareem <monsurkareem@gmail.com>
2021-10-21 16:53:01 +0100

    newly done

--- src/ChapterSeven/SalesCommissionCalculator.java
+++ src/ChapterSeven/SalesCommissionCalculator.java
@@ -1,56 +1,57 @@
-//package ChapterSeven;
-//
-//import java.util.Scanner;
-//
-//public class SalesCommissionCalculator {
-//    public static void main(String[] args) {
-//        Scanner input = new Scanner(System.in);
-//        System.out.println("Enter number of workers: ");
-//        int numberOfWorkers = input.nextInt();
-//
-//        double [] grossPay = new double[numberOfWorkers];
-//
-//
-//        System.out.println("Enter gross pay of workers :");
-//        int i = 0;
-//        for (i = 0; i< grossPay.length; i++){
-//            double salesCommission = 200 + (input.nextInt() * 0.09);
-//            grossPay[i] = salesCommission;
-//        }
-//        if(salesCommission >= 200 || salesCommission < 300){
-//            counterA = counterA + 1;
-//        }
-//        else if(salesCommission >= 300 || salesCommission < 400){
-//            counterB = counterB + 1;
-//        }
-//        else if(salesCommission >= 400 || salesCommission < 500){
-//            counterC = counterC + 1;
-//        }
-//        else if(salesCommission >= 500 || salesCommission < 600){
-//            counterD = counterD + 1;
-//        }
-//        else if(salesCommission >= 600 || salesCommission < 700){
-//            counterE = counterE + 1;
-//        }
-//        else if(salesCommission >= 700 || salesCommission < 800){
-//            counterF = counterF + 1;
-//        }
-//        else if(salesCommission >= 800 || salesCommission < 900){
-//            counterF = counterF + 1;
-//        }
-//        else(salesCommision >900)
-//            counterG = counterG + 1;
-//
-//
-//
-//        int counter = 1;
-//        for(Double pay:grossPay){
-//
-//            System.out.print("Worker " + counter + " ");
-//            System.out.print(pay);
-//            counter++;
-//            System.out.println();
-//        }
-//
-//    }
-//}
+package ChapterSeven;
+
+import java.util.Scanner;
+
+public class SalesCommissionCalculator {
+    private static int counterA;
+    private static int counterB;
+    private static int counterC;
+    private static int counterD;
+    private static int counterE;
+    private static int counterF;
+    private static int counterG;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of workers: ");
+        int numberOfWorkers = input.nextInt();
+
+        double[] grossPay = new double[numberOfWorkers];
+
+
+        System.out.println("Enter gross pay of workers :");
+        int i = 0;
+        double salesCommission = 0;
+        for (i = 0; i < grossPay.length; i++) {
+            salesCommission = 200 + (input.nextInt() * 0.09);
+            grossPay[i] = salesCommission;
+        }
+        if (salesCommission >= 200 || salesCommission < 300) {
+            counterA = counterA + 1;
+        } else if (salesCommission >= 300 || salesCommission < 400) {
+            counterB = counterB + 1;
+        } else if (salesCommission >= 400 || salesCommission < 500) {
+            counterC = counterC + 1;
+        } else if (salesCommission >= 500 || salesCommission < 600) {
+            counterD = counterD + 1;
+        } else if (salesCommission >= 600 || salesCommission < 700) {
+            counterE = counterE + 1;
+        } else if (salesCommission >= 700 || salesCommission < 800) {
+            counterF = counterF + 1;
+        } else if (salesCommission >= 800 || salesCommission < 900) {
+            counterF = counterF + 1;
+      //  } else (salesCommission > 900) {
+       //     counterG = counterG + 1;
+        }
+
+        int counter = 1;
+        for (Double pay : grossPay) {
+
+            System.out.print("Worker " + counter + " ");
+            System.out.print(pay);
+            counter++;
+            System.out.println();
+        }
+
+    }
+}
