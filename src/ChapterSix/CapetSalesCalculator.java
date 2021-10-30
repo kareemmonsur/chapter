7653e5b192a60acd8310a8e9f98cae98667016b2
Monsur Kareem <monsurkareem@gmail.com>
2021-10-30 07:17:34 +0100

    newly done

---
+++ src/ChapterSix/CapetSalesCalculator.java
@@ -0,0 +1,21 @@
+package ChapterSix;
+
+//The width of a carpet
+//        ranges from 11 to 20 ft with an increment of 1 ft. The length of a carpet ranges
+//        from 5 to 25 ft with an increment of 5 ft. The unit price of a carpet is $19 per
+//        square foot.
+
+public class CapetSalesCalculator {
+    public static void main(String[] args) {
+        for(int length = 5; length < 25; length++){
+            for(int width = 11; width < 20; width++){
+                int price = length * width * 19;
+                System.out.print(" " + price);
+            }
+            System.out.println("");
+        }
+
+
+    }
+
+}
