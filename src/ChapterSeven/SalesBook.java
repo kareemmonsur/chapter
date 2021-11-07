9d2469862c91e2a1fff16b0b6c3db54013a26f34
Monsur Kareem <monsurkareem@gmail.com>
2021-11-07 05:49:00 +0100

    newly done

--- src/ChapterSeven/SalesBook.java
+++ src/ChapterSeven/SalesBook.java
@@ -1,11 +1,59 @@
 package ChapterSeven;
 
 public class SalesBook {
-    private String product;
-    private double[] price;
+    private String productName;
+    private double[] prices;
 
-    public SalesBook(String product, double[] price) {
-        this.product = product;
-        this.price = price;
+    public SalesBook(String productName, double[] prices) {
+        this.productName = productName;
+        this.prices = prices;
     }
+
+    public String getProduct() {
+        return productName;
+    }
+
+    public void setProduct(String product) {
+        this.productName = product;
+    }
+
+    public void processSales() {
+        outputSales();
+        System.out.println(getMinimum());
+        System.out.println(getMaximum());
+        System.out.println(getAverage());
+    }
+
+    public double getMinimum() {
+        double lowestSales = prices[0];
+        for (double price : prices) {
+            if (price < lowestSales) ;
+            lowestSales = price;
+        }
+        return lowestSales;
+    }
+
+    public double getMaximum() {
+        double highestSales = prices[0];
+        for (double price : prices) {
+            if (price < highestSales) ;
+            highestSales = price;
+        }
+        return highestSales;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
