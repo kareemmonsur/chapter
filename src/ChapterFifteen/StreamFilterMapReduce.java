b19817f846bcc7e9a2590bd12df33ce318bbbe18
Monsur Kareem <monsurkareem@gmail.com>
2021-11-24 11:12:40 +0100

    newly done

--- src/ChapterFifteen/StreamFilterMapReduce.java
+++ src/ChapterFifteen/StreamFilterMapReduce.java
@@ -4,6 +4,9 @@ import java.util.stream.IntStream;
 
 public class StreamFilterMapReduce {
     public static void main(String[] args) {
-        System.out.printf("Sum of triples of the even ints from 2 to 10: %d%n", IntStream.rangeClosed(1, 10).filter(x -> x% 2 == 0).map(x -> x * 3).sum());
+        System.out.printf("Sum of triples of the even ints from 2 to 10: %d%n",
+                IntStream.rangeClosed(1, 10)
+                        .filter(x -> x% 2 == 0)
+                        .map(x -> x * 3).sum());
     }
 }
--- src/ChapterFifteen/StreamMapReduce.java
+++ src/ChapterFifteen/StreamMapReduce.java
@@ -4,6 +4,6 @@ import java.util.stream.IntStream;
 
 public class StreamMapReduce {
     public static void main(String[] args) {
-        System.out.printf("sum of even int is: %d%n", IntStream.rangeClosed(1,10).map((int x)) -> {return x * 2;}).sum());
+        System.out.printf("sum of even ints from 2 through 20 is: %d%n", IntStream.rangeClosed(1,10).map((int x) -> {return x * 2;}).sum());
     }
 }
